7d4e1bb35a3b8bae9f50b210920ba47c58beb66f
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-11-05 13:32:38 +0530

    Completed The Project !!

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"time"
@@ -46,43 +47,38 @@ func getURL(id string) (URL, error) {
 	return url, nil
 }
 
-func RootPageURL(c *fiber.Ctx) error {
-	return c.SendString("Hello, world!")
-}
-
-func ShortURLHandler(c *fiber.Ctx) error {
-	var data struct {
-		URL string `json:"url"`
-	}
-	if err := c.BodyParser(&data); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid request body",
-		})
-	}
-
-	shortURL := createURL(data.URL)
-	return c.JSON(fiber.Map{
-		"short_url": shortURL,
-	})
-}
-
-func redirectURLHandler(c *fiber.Ctx) error {
-	id := c.Params("id")
-	url, err := getURL(id)
-	if err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error": "URL not found",
-		})
-	}
-	return c.Redirect(url.OriginalURL, fiber.StatusFound)
-}
-
 func main() {
 	app := fiber.New()
 
-	app.Get("/", RootPageURL)
-	app.Post("/shorten", ShortURLHandler)
-	app.Get("/redirect/:id", redirectURLHandler)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, world!")
+	})
+
+	app.Post("/shorten", func(c *fiber.Ctx) error {
+		var data struct {
+			URL string `json:"url"`
+		}
+
+		if err := json.Unmarshal(c.Body(), &data); err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid request body")
+		}
+
+		shortURL := createURL(data.URL)
+		response := struct {
+			ShortURL string `json:"short_url"`
+		}{ShortURL: shortURL}
+
+		return c.Status(fiber.StatusOK).JSON(response)
+	})
+
+	app.Get("/redirect/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		url, err := getURL(id)
+		if err != nil {
+			return c.Status(fiber.StatusNotFound).SendString("URL not found")
+		}
+		return c.Redirect(url.OriginalURL, fiber.StatusFound)
+	})
 
 	fmt.Println("Starting server on port 3000...")
 	if err := app.Listen(":3000"); err != nil {
